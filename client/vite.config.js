e07dd74bd75255aa959a360833a24b2a4c403e72
gokul-1998 <gokulakrishnanm1998@gmail.com>
2024-09-02 15:06:43 +0530

    Added configs

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,8 +6,11 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
-        secure: false,
+        // target: 'http://localhost:3000',
+
+        target: 'https://mern-blogg-mt0d.onrender.com',
+        changeOrigin: true, // Needed to change the origin of the host header to the target URL
+        secure: true,
       },
     },
   },
