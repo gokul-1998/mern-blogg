f6ddb62948b4d672ce9aaa28d8d798eaa6840b2d
gokul-1998 <gokulakrishnanm1998@gmail.com>
2024-09-02 19:05:16 +0530

    ASdasd

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,14 +1,14 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
-      '/api': {
+      "/api": {
         // target: 'http://localhost:3000',
 
-        target: 'https://mern-blogg-ivw5.onrender.com',
+        target: "https://mern-blogg-6mzg.onrender.com",
         changeOrigin: true, // Needed to change the origin of the host header to the target URL
         secure: true,
       },
