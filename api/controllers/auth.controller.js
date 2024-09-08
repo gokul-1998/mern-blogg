fa2e95664484beb2a6623312f6d8d0c9d34670ee
gokul-1998 <gokulakrishnanm1998@gmail.com>
2024-09-08 12:07:56 +0530

    added username in jwt

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -36,7 +36,7 @@ export const signin = async (req, res, next) => {
     if (!validPassword) {
       return next(errorHandler(400, 'Invalid password'));
     }
-    const token = jwt.sign({ id: validUser._id, isAdmin: validUser.isAdmin }, process.env.JWT_SECRET);
+    const token = jwt.sign({ id: validUser._id, isAdmin: validUser.isAdmin ,username:validUser.username}, process.env.JWT_SECRET);
     const { password: pass, ...rest } = validUser._doc;
     res.status(200).cookie('access_token', token, { httpOnly: true }).json(rest);
   } catch (error) {
